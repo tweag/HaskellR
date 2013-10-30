b9e7a7e77a4bc526ebc568f856c2bd3b82df5d4a
Alexander Vershilov <alexander.vershilov@parsci.com>
2013-10-30 16:37:56 +0400

    Created basic interpretator.
    Interpretator is runned in the background thread and may be
    created by calling:
    
      withRInterperter $ \ch ->
    
    all communication is done via typed channel for message type
    and callback.
    
    Really this approach can be generalized for providing any action
    that should be done inside R.

---
+++ src/Math/R/Interpreter.hs
@@ -0,0 +1,80 @@
+-- |
+-- Module: Math.R.Interpreter 
+-- Copyright: (C) $copyright
+--
+-- This module provides a way to run R-interpreter 
+-- in the background thread and interact with it.
+module Math.R.Interpreter 
+  where
+
+import Control.Concurrent.Async ( async, cancel )
+import Control.Concurrent.STM ( atomically
+                              , TChan, newTChanIO, newTChanIO, readTChan, writeTChan
+                              , TMVar, newEmptyTMVarIO, takeTMVar, putTMVar )
+import Control.Exception ( bracket, evaluate )
+import Control.Monad ( void, forever )
+
+import Foreign ( poke, peek, alloca, allocaArray )
+import Foreign.C ( newCString )
+import System.Environment ( getProgName )
+
+import qualified Math.R.Foreign.Embedded as R
+import qualified Math.R.Foreign.Internal as R
+import qualified Math.R.Foreign.Parse    as R
+
+
+data RRequest   =
+        ReqParse String (R.SEXP -> IO ())
+data RError     = RError                
+
+-- | Run interpretator in background thread
+withRInterpret :: (TChan RRequest -> IO a)  -- ^ actions to run
+               -> IO a
+withRInterpret f =
+    bracket 
+      (do ch <- newTChanIO
+          tid <- async $ interpret ch
+          return (ch, tid))
+      (cancel . snd)
+      (f . fst)
+
+interpret :: TChan RRequest -> IO ()
+interpret ch = bracket startEmbedded endEmbedded (const go)
+  where
+    startEmbedded = do
+        pn <- getProgName
+        -- TODO: it's possible to populate with other options
+        allocaArray 1 $ \a -> do
+            sv <- newCString pn
+            poke a sv
+            R.initEmbeddedR 1 a
+        poke R.rInteractive 0
+    endEmbedded _ = void $ R.endEmbeddedR 0
+    go = do
+        rNil <- peek R.nilValue
+        forever $ do
+            req <- atomically $ readTChan ch
+            case req of
+              ReqParse str callback -> do
+                 protect (R.mkString str) $ \tmp ->
+                    alloca $ \status ->
+                       protect (R.parseVector tmp (-1) status rNil) $ \e ->
+                       evaluate $ callback e
+
+parseFile :: TChan RRequest -> FilePath -> (R.SEXP -> a) -> IO a 
+parseFile ch fl f = do
+    box <- newEmptyTMVarIO
+    str <- readFile fl
+    let clb sexp = atomically $ putTMVar box (f sexp)
+    atomically $ writeTChan ch (ReqParse str clb)
+    atomically $ takeTMVar box
+
+
+protect :: (IO R.SEXP) -> (R.SEXP -> IO a) -> IO a
+protect exp f = do
+   e <- exp
+   R.protect e
+   x <- f e
+   R.unprotect 1
+   return x
+
