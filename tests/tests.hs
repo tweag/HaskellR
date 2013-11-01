62e2b19084f8fba4a61a26c4c350de51af644ad4
Mathieu Boespflug <mathieu.boespflug@parsci.com>
2013-11-01 01:03:09 +0100

    Test suite comparing H to R output.
    For each given R source script, the test suite feeds it into R, then
    into H, and compares all lines that print values, to see whether they
    match.
    
    The output postprocessing will need some adjustment in the future, but
    this should be good enough for now.

---
+++ tests/tests.hs
@@ -0,0 +1,75 @@
+-- |
+-- Copyright: (C) 2013 Amgen, Inc.
+--
+-- Tests. Run H on a number of R programs of increasing size and complexity,
+-- comparing the output of H with the output of R.
+
+module Main where
+
+import qualified H
+
+import Test.Tasty hiding (defaultMain)
+import Test.Tasty.Golden.Advanced
+import Test.Tasty.Golden.Manage
+
+import System.IO
+import System.Process
+import System.FilePath
+
+import Control.Monad.Trans
+import Control.Applicative ((<$>))
+import           Data.Text (Text)
+import qualified Data.Text    as T
+import qualified Data.Text.IO as T
+
+invokeR :: FilePath -> ValueGetter r Text
+invokeR fp = do
+    inh <- liftIO $ openFile fp ReadMode
+    (_, Just outh, _, _) <- liftIO $ createProcess $ (proc "R" ["--vanilla"])
+      { std_out = CreatePipe
+      , std_in = UseHandle inh
+      }
+    liftIO $ T.pack <$> hGetContents outh
+
+invokeH :: FilePath -> ValueGetter r Text
+invokeH fp =
+    -- TODO once translated, Haskell source code should be executed somehow and
+    -- output collected.
+    liftIO $ H.translate <$> T.readFile fp
+
+scriptCase :: TestName
+           -> FilePath
+           -> TestTree
+scriptCase name scriptPath =
+    goldenTest
+      name
+      (invokeR scriptPath)
+      (invokeH scriptPath)
+      (\outputR outputH ->
+         if grepRValues (T.lines outputR) == T.lines outputH
+         then return Nothing
+         else return $ Just "Outputs don't match.")
+      (const $ return ())
+  where
+    -- | Filter printed values from R session.
+    grepRValues :: [Text] -> [Text]
+    grepRValues = filter (T.isPrefixOf "[1]")
+
+integrationTests :: TestTree
+integrationTests = testGroup "Integration tests"
+  [ scriptCase "Trivial (empty) script" $
+      "tests" </> "R" </> "empty.R"
+  -- TODO: enable in relevant topic branches.
+  -- , scriptCase "Simple arithmetic" $
+  --     "tests" </> "R" </> "arith.R"
+  -- , scriptCase "Functions - factorial" $
+  --     "tests" </> "R" </> "fact.R"
+  -- , scriptCase "Functions - Fibonacci sequence" $
+  --     "tests" </> "R" </> "fib.R"
+  ]
+
+tests :: TestTree
+tests = testGroup "Tests" [integrationTests]
+
+main :: IO ()
+main = defaultMain tests
