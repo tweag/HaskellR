6ae806fb7d3feee1f1d5d01bb479b3d7bbe0fbb0
Alexander Vershilov <alexander.vershilov@parsci.com>
2013-11-11 17:22:05 +0400

    Introduce H.Prelude module

---
+++ src/H/Prelude.hs
@@ -0,0 +1,10 @@
+-- |
+-- Copyright: (C) 2013 Amgen, Inc.
+module H.Prelude where
+
+import Data.Some
+import qualified H.HVal as H
+import qualified Foreign.R as R
+
+liftR :: (R.SEXP a -> b) -> Some R.SEXP -> b
+liftR f (Some x) = f (R.SEXP . R.unSEXP $ x)
