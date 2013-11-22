4826d30bdc60d42fa4946aae50a3cb18def128c1
Alexander Vershilov <alexander.vershilov@parsci.com>
2013-11-22 13:03:27 +0400

    Refactor and remove boilerplate

--- src/H/HVal.hs
+++ src/H/HVal.hs
@@ -1,7 +1,6 @@
 -- |
 -- Copyright: 2013 (C) Amgen, Inc
 {-# Language ExistentialQuantification #-}
-{-# LANGUAGE ForeignFunctionInterface #-}
 {-# LANGUAGE FunctionalDependencies #-}
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE PolyKinds  #-}
@@ -21,21 +20,17 @@ module H.HVal
   , rfrac
   ) where
 
-import H.HExp
+import H.Internal.Literal
 import qualified Language.R as R
 import qualified Foreign.R  as R
 
 import Control.Applicative
-import Control.Monad ( forM_ )
-import Foreign ( castPtr, castFunPtr, FunPtr, newForeignPtr_, pokeElemOff )
-import Foreign.C.String ( newCString )
+import Foreign ( castPtr , newForeignPtr_ )
 import System.IO.Unsafe ( unsafePerformIO )
 
 -- Temporary
-import qualified Data.Vector.SEXP as SVector
 import qualified Data.Vector.Storable as V
 import Data.List ( intercalate )
-import Data.Word
 
 -- | Runtime universe of R Values
 data HVal = forall a . SEXP (R.SEXP a)
@@ -89,139 +84,3 @@ rplus  x y = R.r2 "+" x y
 rminus x y = R.r2 "-" x y
 rmult  x y = R.r2 "*" x y
 rfrac  x y = R.r2 "/" x y
-
-
--- | Represents a value that can be converted into S Expression
-class Literal a b | a -> b where
-    mkSEXP :: a -> R.SEXP b
-    fromSEXP :: R.SEXP b -> a
-
-instance Literal Double (R.Vector Double) where
-    mkSEXP x = unsafePerformIO $ do
-      v  <- R.allocVector R.Real 1
-      pt <- R.real v
-      pokeElemOff pt 0 (fromRational . toRational $ x)
-      return v
-
-    fromSEXP s =
-      case hexp s of
-        Real (SVector.Vector v) | V.length v == 1 -> v V.! 0
-        _ -> error "Double expected where some other expression appeared."
-
-instance Literal [Double] (R.Vector Double) where
-    mkSEXP x = unsafePerformIO $ do
-        v  <- R.allocVector R.Real l
-        pt <- R.real v
-        forM_ (zip x [0..]) $ \(g,i) -> do
-          pokeElemOff pt i g
-        return v
-      where
-        l = length x
-
-    fromSEXP s =
-      case hexp s of
-        Real (SVector.Vector v) -> V.toList v
-        _ -> error "[Double] expected where some other expression appeared."
-
-instance Literal String (R.Vector Word8) where
-    mkSEXP x = unsafePerformIO $ R.mkString =<< newCString x
-
-    fromSEXP s =
-      case hexp s of
-        Char (SVector.Vector v) -> map (toEnum . fromEnum) $  V.toList v
-        _ -> error "String expected where some other expression appeared."
-
-
---------------------------------------------------------------------------------
--- Helpers for passing functions pointers between Haskell and R               --
---------------------------------------------------------------------------------
-
--- | A class for functions that can be converted to functions on SEXPs.
-class HFunWrap a b | a -> b where
-    hFunWrap :: a -> b
-
-instance Literal a la => HFunWrap (IO a) (IO (R.SEXP la)) where
-    hFunWrap a = fmap mkSEXP a
-
-instance (Literal a la, HFunWrap b wb)
-         => HFunWrap (a -> b) (R.SEXP la -> wb) where
-    hFunWrap f a = hFunWrap $ f $ fromSEXP a
-
-foreign import ccall "wrapper" wrap0 :: IO (R.SEXP a) -> IO (FunPtr (IO (R.SEXP a)))
-
-foreign import ccall "wrapper" wrap1
-    :: (R.SEXP a -> IO (R.SEXP b)) -> IO (FunPtr (R.SEXP a -> IO (R.SEXP b)))
-
-foreign import ccall "wrapper" wrap2
-    :: (R.SEXP a -> R.SEXP b -> IO (R.SEXP c))
-    -> IO (FunPtr (R.SEXP a -> R.SEXP b -> IO (R.SEXP c)))
-
-foreign import ccall "wrapper" wrap3
-    :: (R.SEXP a -> R.SEXP b -> R.SEXP c -> IO (R.SEXP d))
-    -> IO (FunPtr (R.SEXP a -> R.SEXP b -> R.SEXP c -> IO (R.SEXP d)))
-
-foreign import ccall "wrapper" wrap4
-    :: (R.SEXP a -> R.SEXP b -> R.SEXP c -> R.SEXP d -> IO (R.SEXP e))
-    -> IO (FunPtr (R.SEXP a -> R.SEXP b -> R.SEXP c -> R.SEXP d -> IO (R.SEXP e)))
-
-foreign import ccall "wrapper" wrap5
-    :: (  R.SEXP a -> R.SEXP b -> R.SEXP c
-       -> R.SEXP d -> R.SEXP e -> IO (R.SEXP f)
-       )
-    -> IO (FunPtr (  R.SEXP a -> R.SEXP b -> R.SEXP c
-                  -> R.SEXP d -> R.SEXP e -> IO (R.SEXP f)
-                  )
-          )
-
-foreign import ccall "wrapper" wrap6
-    :: (  R.SEXP a -> R.SEXP b -> R.SEXP c
-       -> R.SEXP d -> R.SEXP e -> R.SEXP f -> IO (R.SEXP g)
-       )
-    -> IO (FunPtr (  R.SEXP a -> R.SEXP b -> R.SEXP c
-                  -> R.SEXP d -> R.SEXP e -> R.SEXP f -> IO (R.SEXP g)
-                  )
-          )
-
-foreign import ccall "missing_r.h funPtrToSEXP" funPtrToSEXP
-    :: FunPtr () -> IO (R.SEXP R.Any)
-
-funToSEXP :: HFunWrap a b => (b -> IO (FunPtr b)) -> a -> R.SEXP c
-funToSEXP w x = unsafePerformIO $ fmap castPtr . funPtrToSEXP . castFunPtr
-                =<< w (hFunWrap x)
-
-instance Literal a b => Literal (IO a) R.ExtPtr where
-    mkSEXP = funToSEXP wrap0
-    fromSEXP = error "Unimplemented. fromSEXP (IO a)"
-
-instance (Literal a a0, Literal b b0) => Literal (a -> IO b) R.ExtPtr where
-    mkSEXP = funToSEXP wrap1
-    fromSEXP = error "Unimplemented. fromSEXP (a -> IO b)"
-
-instance (Literal a a0, Literal b b0, Literal c c0)
-         => Literal (a -> b -> IO c) R.ExtPtr where
-    mkSEXP = funToSEXP wrap2
-    fromSEXP = error "Unimplemented. fromSEXP (a -> b -> IO c)"
-
-instance (Literal a a0, Literal b b0, Literal c c0, Literal d d0)
-         => Literal (a -> b -> c -> IO d) R.ExtPtr where
-    mkSEXP = funToSEXP wrap3
-    fromSEXP = error "Unimplemented. fromSEXP (a -> b -> c -> IO d)"
-
-instance (Literal a a0, Literal b b0, Literal c c0, Literal d d0, Literal e e0)
-         => Literal (a -> b -> c -> d -> IO e) R.ExtPtr where
-    mkSEXP = funToSEXP wrap4
-    fromSEXP = error "Unimplemented. fromSEXP (a -> b -> c -> d -> IO e)"
-
-instance ( Literal a a0, Literal b b0, Literal c c0, Literal d d0, Literal e e0
-         , Literal f f0
-         )
-         => Literal (a -> b -> c -> d -> e -> IO f) R.ExtPtr where
-    mkSEXP = funToSEXP wrap5
-    fromSEXP = error "Unimplemented. fromSEXP (a -> b -> c -> d -> e -> IO f)"
-
-instance ( Literal a a0, Literal b b0, Literal c c0, Literal d d0, Literal e e0
-         , Literal f f0, Literal g g0
-         )
-         => Literal (a -> b -> c -> d -> e -> f -> IO g) R.ExtPtr where
-    mkSEXP = funToSEXP wrap6
-    fromSEXP = error "Unimplemented. fromSEXP (a -> b -> c -> d -> e -> f -> IO g)"
---
+++ src/H/Internal/TH.hs
@@ -0,0 +1,79 @@
+module H.Internal.TH
+  ( thWrappers
+  , thWrapper
+  , thWrapperLiteral
+  , thWrapperLiterals
+  ) where
+
+import Language.Haskell.TH
+
+
+-- | Generate wrappers from n to m
+thWrappers :: Int -> Int -> DecsQ
+thWrappers n m = mapM thWrapper [n..m]
+
+-- | Generate wrapper
+--
+-- Example for input 5:
+--
+-- @
+-- foreign import ccall \"wrapper\" wrap5
+--    :: (  R.SEXP a -> R.SEXP b -> R.SEXP c
+--       -> R.SEXP d -> R.SEXP e -> IO (R.SEXP f)
+--       )
+--    -> IO (FunPtr (  R.SEXP a -> R.SEXP b -> R.SEXP c
+--                  -> R.SEXP d -> R.SEXP e -> IO (R.SEXP f)
+--                  )
+--          )
+-- @
+thWrapper :: Int -> DecQ
+thWrapper n = do
+    forImpD cCall safe "wrapper" (mkName $ "wrap"++show n) $
+      forallT (map PlainTV vars) (cxt []) $
+        (appT arrowT (go vars))  `appT` 
+        ( conT (mkName "IO") `appT` (conT (mkName "FunPtr") `appT` (go vars)))
+  where
+    vars :: [Name]
+    vars = take (n+1) $ map (mkName.return) ['a'..]
+    go :: [Name] -> TypeQ
+    go [] = error "thWrapper: impossible happened"
+    go [x] = 
+      conT (mkName "IO") `appT` (conT (mkName "R.SEXP") `appT` (varT x))
+    go (x:xs) = 
+      (appT arrowT (conT (mkName "R.SEXP") `appT` (varT x)))
+        `appT` (go xs)
+
+thWrapperLiterals :: Int -> Int -> DecsQ
+thWrapperLiterals n m = mapM thWrapperLiteral [n..m]
+
+-- | Generate Literal Instance for wrapper
+-- 
+-- Example for input 6
+-- @
+-- instance ( Literal a a0, Literal b b0, Literal c c0, Literal d d0, Literal e e0
+--         , Literal f f0, Literal g g0
+--         )
+--         => Literal (a -> b -> c -> d -> e -> f -> IO g) R.ExtPtr where
+--    mkSEXP = funToSEXP wrap6
+--    fromSEXP = error \"Unimplemented. fromSEXP (a -> b -> c -> d -> e -> f -> IO g)\"
+-- @
+thWrapperLiteral :: Int -> DecQ
+thWrapperLiteral n = instanceD ctx typ funs
+  where 
+    vars :: [Name]
+    vars = take (n+1) $ map (mkName.return) ['a'..]
+    vars0 :: [Name]
+    vars0 = take (n+1) $ map (\i -> mkName [i,'0']) ['a'..]
+    tps []  = error "impossible happened"
+    tps [x] = conT (mkName "IO") `appT` (varT x)
+    tps (x:xs) = (appT arrowT (varT x)) `appT` tps xs
+    -- context
+    ctx = cxt (map go (zip vars vars0))
+      where
+        go (k, k0) = classP (mkName "Literal") [varT k, varT k0]
+    -- type
+    typ = (conT (mkName "Literal") `appT` (tps vars) `appT` (conT (mkName "R.ExtPtr")))
+    -- funs
+    funs = [ mk, from ]
+    mk = funD (mkName "mkSEXP") [clause [] (normalB $ appE  (varE (mkName "H.Internal.Literal.funToSEXP")) (varE (mkName ("wrap"++show n)))) []]
+    from = funD (mkName "fromSEXP") [clause [] (normalB $ appE (varE (mkName "error")) (litE (stringL "Unimplemented."))) []]
