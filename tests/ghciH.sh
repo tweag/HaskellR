89e840386b3ec499db73ec78de176b158df4de29
Alexander Vershilov <alexander.vershilov@gmail.com>
2013-12-05 13:44:54 +0400

    Use C locale for tests.

--- tests/ghciH.sh
+++ tests/ghciH.sh
@@ -4,4 +4,4 @@ if command -v ghcii.sh > /dev/null 2>&1
 then
     GHCi=ghcii.sh
 fi
-$GHCi -package-db=dist/package.conf.inplace "$@" 2>&1
+LANG=C $GHCi -package-db=dist/package.conf.inplace "$@" 2>&1
