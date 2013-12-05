96fc9405c11c2a2ab7246028eada9d1a9e3269bc
Alexander Vershilov <alexander.vershilov@gmail.com>
2013-12-05 13:45:06 +0400

    Update coverage test restrict other packages.

--- tests/run-coverage.sh
+++ tests/run-coverage.sh
@@ -1,11 +1,11 @@
-#!/bin/sh
+#!/bin/sh -d
 [ -d .hpc ] || rm -rf .hpc
 cabal clean
-cabal configure --enable-tests --enable-library-profiling --ghc-options -fhpc
+cabal configure --enable-tests --ghc-options -fhpc
 cabal build
-rm *.tix
+rm *.tix > /dev/null
 ./dist/build/tests/tests
 [ -d report ] || mkdir report
-hpc report --exclude=Main H.tix > report/summary.txt
-hpc markup --exclude=Main --srcdir=. H.tix --destdir=report
+hpc markup --include="H-0.1.0.0:" --srcdir=. H.tix --destdir=report
+hpc report --include="H-0.1.0.0:" H.tix > report/summary.txt
 
