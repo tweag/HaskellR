5cde14baf17a2a8776bd9c8aa5af11c503624823
Alexander Vershilov <alexander.vershilov@tweag.io>
2013-12-09 13:00:13 +0400

    Use runInRThread only on the top of Monad R.
    1). Move monadR class to the Class/Ghci as it's not currently used
    anywhere else.
    2). Create runR function that sends execution to the R thread.
    3). Fixup tests and API.

--- src/H/Class/Ghci.hs
+++ src/H/Class/Ghci.hs
@@ -5,13 +5,18 @@
 
 {-# OPTIONS_GHC -fno-warn-orphans #-}
 module H.Class.Ghci
-  ( 
+  ( MonadR(..)
   ) where
 
 import Control.Monad.Reader
 
 import H.Monad
 import H.Internal.REnv
+
+-- This class is intendent for creation and running
+-- H code.
+class MonadR m where
+  runR :: R a -> m a
 
 instance MonadR IO where
   runR (R x) = runReaderT x REnv
--- src/H/Monad.hs
+++ src/H/Monad.hs
@@ -3,7 +3,7 @@
 module H.Monad
   ( R(..)
   , REnv
-  , MonadR(..)
+  , runR
   , io
   ) where
 
@@ -11,13 +11,13 @@ import H.Internal.REnv
 
 import Control.Monad.Reader
 import Control.Monad.Catch
+import Language.R.Interpreter ( runInRThread )
 
 newtype R a = R (ReaderT REnv IO a) deriving (Monad, MonadIO, Functor, MonadCatch)
 
--- This class is intendent for creation and running
--- H code.
-class MonadR m where
-  runR :: R a -> m a
+-- | Execute R monad.
+runR :: REnv -> R a -> IO a
+runR env (R (ReaderT f)) = runInRThread $ f env
 
 io :: IO a -> R a
 io = liftIO
