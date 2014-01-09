a3002ba7196ff44426695ea90a35c1662c7cbd85
Mathieu Boespflug <m@tweag.io>
2014-01-09 23:48:12 +0100

    Fix documentation in Language.R.GC.

--- src/Language/R/GC.hs
+++ src/Language/R/GC.hs
@@ -1,11 +1,12 @@
 -- |
 -- Copyright: (C) 2013 Amgen, Inc.
 --
--- This module provides helper functions and structures for
--- safe memory operations for communication with R thread.
+-- This module provides helper functions and structures for safe memory
+-- management when communicating with R.
 --
--- RVal is a value supposed to hold SEXP that are in haskell
--- memory from garbage collection on in R.
+-- 'RVal' is a wrapper around 'SEXP' preventing the 'SEXP' from being garbage
+-- collected by R until the Haskell garbage collector signals that it is safe to
+-- do so.
 {-# LANGUAGE GADTs #-}
 module Language.R.GC
   ( -- * RVal
@@ -33,24 +34,23 @@ import qualified Foreign.R as R
 
 import Control.Exception ( bracket )
 
--- | The type "RVal" represent reference to R object that is
--- maintained by R storage memory. RValue automatically 'Foreign.R.protect'
--- object and 'Foreign.R.unprotectPtr' it when it becomes unavailable.
-data RVal :: R.SEXPTYPE -> * where
-  RVal :: ForeignPtr R.SEXPREC -> RVal a
+-- | An 'RVal' is a reference to a /protected/ R object that is maintained by
+-- R storage memory. If GHC's GC determines that the object has become
+-- unreachable from Haskell, then object is unprotected.
+newtype RVal (a :: R.SEXPTYPE) = RVal (ForeignPtr R.SEXPREC)
 
--- | Create R value and automatically protect it
+-- | Create R value and automatically protect it.
 newRVal :: MonadR m => R.SEXP a -> m (RVal a)
 newRVal s = io $ do
     _ <- R.protect s
     fp <- newForeignPtr (R.unsexp s) (R.unprotectPtr s)
     return (RVal fp)
 
--- | Keep SEXP value from the garbage collection
+-- | Keep SEXP value from the garbage collection.
 touchRVal :: MonadR m => RVal a -> m ()
 touchRVal (RVal s) = io (touchForeignPtr s)
 
--- | This is a way to look inside RValue object
+-- | This is a way to look inside RValue object.
 withRVal :: MonadR m => RVal a -> (R.SEXP a -> m b) -> m b
 withRVal (RVal s) f = do
         let s' = unsafeForeignPtrToPtr s
@@ -58,7 +58,7 @@ withRVal (RVal s) f = do
         io $ touchForeignPtr s
         return x
 
--- | Unprotect "SEXP" in R memory. This doesn't mean that value will be
+-- | Unprotect 'SEXP' in R memory. This doesn't mean that value will be
 -- immideately deallocated, just that it may be deallocated on the next GC.
 unprotectRVal :: MonadR m => RVal a -> m ()
 unprotectRVal (RVal s) = io (finalizeForeignPtr s)
@@ -73,31 +73,29 @@ withProtected :: IO (R.SEXP a)      -- Action to acquire resource
 withProtected accure =
    bracket (accure >>= \x -> R.protect x >> return x)
            (const (R.unprotect 1))
-           
--- | The type "RVal" represent reference to R object that is
--- maintained by R storage memory. RValue automatically 'Foreign.R.protect'
--- object and 'Foreign.R.unprotectPtr' it when it becomes unavailable.
-data SomeRVal = SomeRVal (ForeignPtr R.SEXPREC)
 
--- | Create R value and automatically protect it
+-- | Non type indexed version of 'RVal'.
+newtype SomeRVal = SomeRVal (ForeignPtr R.SEXPREC)
+
+-- | Create R value and automatically protect it.
 newSomeRVal :: MonadR m => SomeSEXP -> m SomeRVal
 newSomeRVal (SomeSEXP s) = io $ do
     _ <- R.protect s
     SomeRVal <$> newForeignPtr (R.unsexp s) (R.unprotectPtr s)
 
--- | Keep SEXP value from the garbage collection
+-- | Keep 'SEXP' value from the garbage collection.
 touchSomeRVal :: MonadR m => SomeRVal -> m ()
 touchSomeRVal (SomeRVal s) = io (touchForeignPtr s)
 
--- | This is a way to look inside RValue object
+-- | This is a way to look inside RValue object.
 withSomeRVal :: MonadR m => SomeRVal -> (SomeSEXP -> m b) -> m b
-withSomeRVal (SomeRVal s) f = do 
+withSomeRVal (SomeRVal s) f = do
         let s' = unsafeForeignPtrToPtr s
         x <- f (SomeSEXP (R.sexp s'))
         io $ touchForeignPtr s
         return x
 
--- | Unprotect "SEXP" in R memory. This doesn't mean that value will be
+-- | Unprotect 'SEXP' in R memory. This doesn't mean that value will be
 -- immideately deallocated, just that it may be deallocated on the next GC.
 unprotectSomeRVal :: MonadR m => SomeRVal -> m ()
 unprotectSomeRVal (SomeRVal s) = io (finalizeForeignPtr s)
