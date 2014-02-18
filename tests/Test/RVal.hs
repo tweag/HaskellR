5ac382cebe54a6f9786d41c3afcc9f9e888c4a01
Mathieu Boespflug <m@tweag.io>
2014-02-18 23:21:18 +0100

    Remove one gc test.
    Testing that GC did happen cannot be tested reliably, and indeed this
    test succeeded on some distributions and failed on others. This is
    because the freed memory might not be reinitialized.

--- tests/Test/RVal.hs
+++ tests/Test/RVal.hs
@@ -14,12 +14,7 @@ import Test.Tasty.HUnit
 
 tests :: TestTree
 tests = testGroup "HVal"
-    [ testCase "SEXP is collected after R GC" $ do
-        ((assertBool "SEXP was not collected" . not . isInt) =<<) $ do
-          x <- R.allocVector SingR.SInt 1024 :: IO (R.SEXP R.Int)
-          R.gc
-          return $ R.typeOf x
-    , testCase "RVal is not collected by R GC" $ do
+    [ testCase "RVal is not collected by R GC" $ do
         ((assertBool "RVal was collected" . isInt) =<<) $ do
             -- double initialization, but it's safe
             R.runR R.defaultConfig $ do
