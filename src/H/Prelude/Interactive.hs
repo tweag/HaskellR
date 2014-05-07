c523bbc80e545b525cfa4de560b6b99c9897380d
Alexander Vershilov <alexander.vershilov@tweag.io>
2014-05-07 15:33:22 +0400

    Update R interactive to fix import error.

--- src/H/Prelude/Interactive.hs
+++ src/H/Prelude/Interactive.hs
@@ -4,18 +4,18 @@
 -- This class is not meant to be imported in any other circumstance than in
 -- a GHCi session.
 
+{-# OPTIONS_GHC -fno-warn-orphans #-}
 module H.Prelude.Interactive
   ( module H.Prelude
-  , module Language.R.Runtime.QQ
   , printQuote
   )
   where
 
 import H.Prelude hiding (runR)
-import Language.R.Runtime.QQ
+import Language.R.Runtime.QQ ()
 import qualified H.Prelude as H
 
 -- | A form of the 'print' function that that is more convenient in an
 -- interactive session.
-printQuote :: H.Show a => IO a -> IO ()
+printQuote :: (MonadR m, H.Show a) => m a -> m ()
 printQuote = (>>= H.print)
