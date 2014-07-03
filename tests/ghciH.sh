6d11d5cf7314be0d727256a1b8cdc568674fa0da
Facundo Domínguez <facundo.dominguez@tweag.io>
2014-07-03 19:37:47 -0300

    Remove unsafePerformIO calls from Language.R.QQ.
    This eliminates the need to specify -fno-full-lazyness and makes the
    code resilient to common subexpression elimination as well.

--- tests/ghciH.sh
+++ tests/ghciH.sh
@@ -11,7 +11,7 @@ fi
 # NOTE: We must make both the dependencies in the sandbox and the H package
 # in the dist directory available to GHCi.
 if package_db=`find_sandbox_package_db`; then
-	LANG=C ${GHCi} -fno-full-laziness -no-user-package-db -package-db="${package_db}" -package-db=dist/package.conf.inplace $GHCi_H_ARGS "$@" 2>&1
+	LANG=C ${GHCi} -no-user-package-db -package-db="${package_db}" -package-db=dist/package.conf.inplace $GHCi_H_ARGS "$@" 2>&1
 else
-	LANG=C ${GHCi} -fno-full-laziness -package-db=dist/package.conf.inplace $GHCi_H_ARGS "$@" 2>&1
+	LANG=C ${GHCi} -package-db=dist/package.conf.inplace $GHCi_H_ARGS "$@" 2>&1
 fi
