715731c306abd7a241fa4f10b5f6999bd2f7c848
Mathieu Boespflug <m@tweag.io>
2014-07-18 21:27:59 +0200

    Reflow and rewrite documentation of C.M.R module.

--- src/Control/Monad/R.hs
+++ src/Control/Monad/R.hs
@@ -1,57 +1,44 @@
 -- |
 -- Copyright: (C) 2013 Amgen, Inc.
 --
--- Monad R provides a safe way to work with R object, this monad performs all required
--- variable protection under the hood. The approach to the regions is a simplified
--- lightweigh regions.
+-- = The R monad
 --
--- Inside R monad it's possible to protect 'Foreign.R.Internal.SEXP' by adding them into R protection stack
--- and once block is finished all values will be unprotected in one call. This basically
--- doesn't mean that values will be freed by R runtime, but it will not be safe to access
--- them. R monad also solves a problem of 'leaking' resources, so no value that is protected
--- withing a block can leave block scope.
+-- The R Monad provides a safe way to work with R values. The R monad manages
+-- protection of values under the hood, so that the R garbage collector does not
+-- deallocate them prematurely.
 --
--- All values that should be protected in a scope of 'R' monad should contain @s@ type variable
--- see 'Foreign.R' for an example.
+-- The idea is to track allocation of R values within a /region/. Within the
+-- scope of the region, values are protected and hence guaranteed not to be
+-- freed. Once control flow exits the region, the values allocated within that
+-- region are no longer protected, and hence susceptible to being freed by R's
+-- garbage collector.
 --
--- It's possible to run a protection block, all resources that are allocated withing a
--- block are protected by calling 'protect' and once block is finished, then all
+-- The current region is identified by a dummy index type variable @s@ to the
+-- R monad. That is, in the type @R s a@, @s@ marks the current region. Use
+-- 'runRegion' to open a new region, e.g.
 --
 -- > runRegion $ do
 -- >   x <- fromSEXP <$> protect foo
 -- >   y <- fromSEXP <$> protect bar
 -- >   foo (x,y)
 --
--- Here @x@ and @y@ will be protected during the computation of @foo@ and will be unprotected
--- just before a return.
+-- Here @x@ and @y@ will be protected during the computation of @foo@ and will
+-- be unprotected just before a return.
 --
--- Implementation notes:
+-- __Implementation notes:__
 --
---   * The current solution does not support nested regions, so passing a value to the
---     child region from a parent is not possible even it's safe.
+--   * The current solution does not support nested regions, so passing a value
+--   to the child region from a parent is not possible even it's safe.
 --
--- Protection of values:
+-- = Liberating values
 --
--- Any value can be protected by using 'protect' function from the 'Protect' typeclass, this
--- function guarantees that value is protected withing a region and can be used there.
---
--- Once a value is leaving region, either by a final statement or by passing into any communication
--- channel it should be unprotected, so to be prepared to be used outside a region. This involve
--- 3 things:
---
---    1. Value will change type to the type that doesn't contain a reference to a region;
---
---    2. Value will be marked as 'UnsafeValue' if it's not safe to use without a protection.
---    (see 'UnsafeValue' for complete documentation)
---
---    3. Value is fully evaluated, so it doesn't contain any thunks to a variables that are
---    safe to use only inside a region.
---
--- It's still safe to use original value inside a region even after 'unprotection' procedure.
--- 'runRegion' procedure takes care of unprotection procedure, however user should do it himself
--- before passing a value into the communication channel.
---
+-- All R values are bound to a region. However, it is sometimes necessary to
+-- communicate a value to remote threads, which will not in general be part of
+-- the same region. This is possible once a value is *liberated* from its native
+-- region.
+
 {-# LANGUAGE Rank2Types #-}
+
 module Control.Monad.R
   ( -- * The R monad
     R
