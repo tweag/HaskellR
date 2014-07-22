501a630bee5240ed916dde01d461e449a9fd47d8
Alexander Vershilov <alexander.vershilov@gmail.com>
2014-07-22 11:52:10 +0400

    Introduce an IsAncestor relation
    IsAncestor relation introduce an 'ordering' between regions, if
    'a `IsAncestor` b' holds, then all values from region a, can be
    used (cast) to region b. This type family is useful even in absence
    of nested regions, because even without nesting we have 3 different
    types of region: V, G, s. With the ordering: forall s: V < s < G.
    This addition allows to introduce a generic 'release' function, so
    actions that takes a 'SEXP' from a different regions may be run in
    a region rather that 'V'.
    
    It should be noted that this API doesn't introduce any additional
    complexity over previous solution, as for all cases where non generic
    'release' can be used 'IsAncestor' will be automatically derived,
    plus this method can be used in places where we need to loosen region
    from the 'G' to 's', thats is safe but old API required to use
    'unsafeRelease' that is a temporary solution and should be removed
    in the final version.

--- src/Control/Memory/Region.hs
+++ src/Control/Memory/Region.hs
@@ -1,10 +1,20 @@
 -- |
 -- Copyright: (C) 2013 Amgen, Inc.
 
+{-# LANGUAGE TypeFamilies #-}
 module Control.Memory.Region where
+
 
 data GlobalRegion
 data Void
 
 type G = GlobalRegion
 type V = Void
+
+
+-- | Relaction describes if Regions @a@ is ancestor
+-- of region @b@. In terms of regions this means that
+-- Any value from @a@ can safely be used in @b@.
+type family   IsAncestor a b :: Bool
+type instance IsAncestor G x = True
+type instance IsAncestor x V = True
