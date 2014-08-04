87803642939e678ef7addaf5d1dac6b5f95ae700
Alexander Vershilov <alexander.vershilov@gmail.com>
2014-08-04 16:57:36 +0400

    Remove non-required tests

--- tests/Test/HExp.hs
+++ tests/Test/HExp.hs
@@ -20,26 +20,4 @@ tests = testGroup "hexp"
               R.withProtected (H.selfSymbol chr) $ \slf -> do
                 assertBool "selfSymbol==selfSymbol" (H.hexp slf === H.hexp slf)
         ]
-    , testGroup "HExp-support-unknown types"
-        [ testCase "equal" $ do
-            s1 <- unsafeRToIO [r| 1 |]
-            s2 <- unsafeRToIO [r| 1 |]
-            unSomeSEXP s1 $ \a -> unSomeSEXP s2 $ \b ->
-              assertBool "Can compare" $ H.hexp a === H.hexp b
-        , testCase "not equal" $ do
-            s1 <- unsafeRToIO [r| 1 |]
-            s2 <- unsafeRToIO [r| 2 |]
-            unSomeSEXP s1 $ \a -> unSomeSEXP s2 $ \b ->
-              assertBool "Can compare" $ not $ H.hexp a === H.hexp b
-        , testCase "different types" $ do
-            s1 <- unsafeRToIO [r| 1 |]
-            s2 <- unsafeRToIO [r| as.integer(1) |]
-            unSomeSEXP s1 $ \a -> unSomeSEXP s2 $ \b ->
-              assertBool "Can compare" $ not $ H.hexp a === H.hexp b
-        , testCase "same after evaluation" $ do
-            s1 <- unsafeRToIO [r| 1 |]
-            s2 <- unsafeRToIO [r| 0+1 |]
-            unSomeSEXP s1 $ \a -> unSomeSEXP s2 $ \b ->
-              assertBool "Can compare" $ H.hexp a === H.hexp b
-        ]
-    ]
+  ]
