47bc15e1e1ae9b975111e75b8c49fc3c3f9b7646
Alexander Vershilov <alexander.vershilov@gmail.com>
2014-08-06 10:25:10 +0400

    fixup! Introduce an IsAncestor relation

--- src/Control/Memory/Region.hs
+++ src/Control/Memory/Region.hs
@@ -15,6 +15,6 @@ type V = Void
 -- | Relaction describes if Regions @a@ is ancestor
 -- of region @b@. In terms of regions this means that
 -- Any value from @a@ can safely be used in @b@.
-type family   IsAncestor a b :: Bool
-type instance IsAncestor G x = True
-type instance IsAncestor x V = True
+type family   IsAncestorOf a b :: Bool
+type instance IsAncestorOf G x = True
+type instance IsAncestorOf x V = True
