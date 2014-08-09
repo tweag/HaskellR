3fa3b84f85f7c0b404c9c97dd78849ac961f19fd
Mathieu Boespflug <m@tweag.io>
2014-08-09 13:05:40 +0200

    Overhaul set membership constraints, using new GHC 7.8 features.
    With data kinds, and closed type families, it becomes possible to
    express set membership on the type level as a type family, over type
    level lists using the normal syntax for lists. In this way, we drop the
    use of OverlappingInstances.

--- src/H/Constraints.hs
+++ src/H/Constraints.hs
@@ -3,27 +3,21 @@
 --
 -- Predicates seen in type signatures in H.
 
-{-# LANGUAGE PolyKinds #-}
 {-# LANGUAGE ConstraintKinds #-}
-{-# LANGUAGE OverlappingInstances #-}
+{-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE TypeFamilies #-}
 
 module H.Constraints where
 
+import GHC.Exts (Constraint)
+import {-# SOURCE #-} Foreign.R.Type (SEXPTYPE(..))
+
 infix 1 :∈
-infixr 2 :+:
 
--- | Combine classes. This datatype is used purely as a type index, has no
--- computational significance and hence has no runtime representation.
-data a :+: b
-
--- | Type level lists not nil terminated. This is a syntactic convenience, even
--- if it means we need slightly more instances than would otherwise be needed
--- below. In any case, making empty lists representable would duplicate the lub
--- of all classes (e.g. 'Foreign.R.Any' in R).
-class a :∈ b
-instance a :∈ a
-instance a :∈ (a :+: b)
-instance (a :∈ c) => a :∈ (b :+: c)
+-- | The predicate @a :∈ as@ states that @a@ is a member type of the set @as@.
+type family (a :: SEXPTYPE) :∈ (as :: [SEXPTYPE]) :: Constraint where
+  a :∈ (a ': as) = ()
+  a :∈ (b ': as) = a :∈ as
 
 -- | Class alias used for c2hs @fun@ hooks, since it currently does not like
 -- Unicode operators.
