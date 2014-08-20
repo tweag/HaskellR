ef9cccb4aa3a50927a12c58da232e8903176d9d0
Alexander Vershilov <alexander.vershilov@gmail.com>
2014-08-20 12:42:55 +0400

    Fixed backcompatibility with R-3.0.2.

--- tests/Test/Regions.hs
+++ tests/Test/Regions.hs
@@ -1,5 +1,7 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE CPP #-}
+{-# LANGUAGE ForeignFunctionInterface #-}
 
 module Test.Regions
   ( tests )
@@ -15,28 +17,45 @@ import Foreign
 
 import System.Directory (getCurrentDirectory, setCurrentDirectory)
 import Control.Exception (bracket)
+
 preserveDirectory :: IO a -> IO a
 preserveDirectory =
  bracket getCurrentDirectory setCurrentDirectory . const
 
+-- 196864 - is R-3.1.0
+#if R_VERSION >= 196864
+foreign import ccall "&R_PPStackTop" ppStackTop :: Ptr Int
+
+stackBalanced :: IO () -> IO ()
+stackBalanced f = do
+   i <- peek R.ppStackTop
+   f
+   j <- peek R.ppStackTop
+   assertEqual "protection stack should be balanced" i j
+#else
+stackBalanced :: IO () -> IO ()
+stackBalanced f = f
+#endif
+
 tests :: TestTree
 tests = testGroup "regions"
     [ testCase "qq-dont-leak" $
-      preserveDirectory $ do
-        i <- peek R.ppStackTop
+      preserveDirectory $ stackBalanced $
         runRegion $ do
-          _ <- [r| 1 |]
-          j <- io $ peek R.ppStackTop
-          io $ assertEqual "protection stack should grow" (succ i) j
-        j <- peek R.ppStackTop
-        assertEqual "protection stack should be balanced" i j
+          _ <- [r| gctorture(TRUE) |]
+          R.SomeSEXP x <- [r| 1 |]
+          _ <- io $ R.allocList 1
+          io $ assertEqual "value is protected" R.Real (R.typeOf x)
+          _ <- [r| gctorture(FALSE) |]
+          return ()
     , testCase "mksexp-dont-leak" $
-      preserveDirectory $ do
-        i <- peek R.ppStackTop
+      preserveDirectory $ stackBalanced $
         runRegion $ do
-          _ <- mkSEXP (1::Int32)
-          j <- io $ peek R.ppStackTop
-          io $ assertEqual "protection stack should grow" (succ i) j
-        j <- peek R.ppStackTop
-        assertEqual "protection stack should be balanced" i j
+          _ <- [r| gctorture(TRUE) |]
+          x <- mkSEXP (1::Int32)
+          _ <- io $ R.allocList 1
+          io $ assertEqual "value is protected" R.Int (R.typeOf x)
+          _ <- [r| gctorture(FALSE) |]
+          return ()
     ]
+
