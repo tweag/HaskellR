30a42b7b7764a15a819a3c93783bb655f032cffd
Mateusz Kowalczyk <fuuzetsu@fuuzetsu.co.uk>
2015-07-21 13:58:54 +0100

    Embed H.ghci at compile time
    This makes for a rellocatable binary.
    
    `withSystemTempFile` is used for a simplest solution: trying to pipe it
    into the spawned process' stdin was futile: if we're `Inherit`ing a
    pipe, we don't get a handle from `createProcess`. Attempts at `stdin`
    handle failed with handle not being open to writes: perhaps we were
    trying to write too early. Instead of trying to time it better, we just
    use a temporary file. One caveat is that we have to close the handle
    after writing otherwise the created process can't read it.

--- H/H.hs
+++ H/H.hs
@@ -5,12 +5,17 @@
 {-# LANGUAGE CPP #-}
 {-# LANGUAGE DeriveDataTypeable #-}
 {-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE TemplateHaskell #-}
 
 module Main where
 
+import Data.ByteString (hPut)
+import Data.FileEmbed (embedFile)
 import Data.Version ( showVersion )
 import System.Console.CmdArgs
 import System.Exit (exitWith)
+import System.IO (hClose)
+import System.IO.Temp (withSystemTempFile)
 import System.Process
 
 import qualified Paths_H
@@ -36,8 +41,8 @@ main :: IO ()
 main = do
     config <- cmdArgs cmdSpec
     case config of
-      H {configFiles, configInteractive} -> do
-        cfg  <- Paths_H.getDataFileName "H.ghci"
+      H {configFiles, configInteractive} -> withSystemTempFile "H.ghci" $ \cfg h -> do
+        hPut h $(embedFile "H.ghci") >> hClose h
         let argv = configFiles ++ ["-v0", "-ghci-script", cfg]
         (_,_,_,ph) <-
           createProcess (proc configInteractive argv)
