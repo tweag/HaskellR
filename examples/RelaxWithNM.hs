d9eab889c7ce754fa27f6ff26c7ad9e6c8f14ace
Mateusz Kowalczyk <fuuzetsu@fuuzetsu.co.uk>
2015-07-21 14:29:55 +0100

    Add RelaxWithNM to be part of the stack build

--- examples/RelaxWithNM.hs
+++ examples/RelaxWithNM.hs
@@ -1,11 +1,8 @@
 -- | Contributed by Dominic Steinitz
 
 -- Requirements:
---   Haskell packages: integration
 --   R packages:       optimx
 --                     numDeriv
---   In oder to install Haskell package
---   use stack build or cabal install.
 --
 --   In order to install R packges use
 --   install.packages("package-name") in R
