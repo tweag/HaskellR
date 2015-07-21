09b313dec94bf4d3e3e85de2ee4de3d403ac2950
Mateusz Kowalczyk <fuuzetsu@fuuzetsu.co.uk>
2015-07-21 18:34:05 +0100

    Dummy-package nls examples
    Primarily intended to explicitly specify the dependency on `mwc-random`.
    An alternative approach would be to just feed in the script for the user
    though that poses a question of *how* to do it: do we just assume we can
    run ‘stack exec H’? It seems to defeat the point of the example.

---
+++ examples/nls/Main.hs
@@ -0,0 +1,13 @@
+-- | Dummy module intended to inform the user how this example should
+-- be run.
+module Main where
+
+import System.Exit (exitFailure)
+
+main :: IO ()
+main = do
+  putStrLn $ concat [ "This example is intended to be executed by feeding"
+                    , " the script into the H interpretter, e.g."
+                    , " 'H -- -ghci-script nls.hs'"
+                    ]
+  exitFailure
