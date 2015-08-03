e47a6f4f0c670f21d14b654fb370f59e123a8099
Mathieu Boespflug <m@tweag.io>
2015-08-03 22:33:27 +0200

    Split event test into several independent tests.

--- inline-r/tests/Test/Event.hs
+++ inline-r/tests/Test/Event.hs
@@ -4,10 +4,11 @@
 module Test.Event where
 
 #ifndef mingw32_HOST_OS
-import Data.IORef (newIORef, readIORef, writeIORef)
+import Data.IORef (modifyIORef', newIORef, readIORef, writeIORef)
 import Foreign (FunPtr, Ptr, freeHaskellFunPtr)
 import qualified Foreign.R.EventLoop as R
 import H.Prelude
+import Language.R.Event
 import System.IO (hClose, hPutStrLn)
 import System.IO.Temp (withSystemTempFile)
 import System.Posix.IO
@@ -17,6 +18,7 @@ import System.Posix.IO
   , defaultFileFlags
   , openFd
   )
+import System.Posix.Types (Fd)
 #endif
 import Test.Tasty
 import Test.Tasty.HUnit
@@ -29,20 +31,45 @@ foreign import ccall "wrapper" wrap
 
 tests :: TestTree
 tests = testGroup "events"
-    [
-#ifndef mingw32_HOST_OS
-      testCase "file events (select)" $ do
-        withSystemTempFile "inline-r-" $ \path h -> do
-          hPutStrLn h "hello"
-          hClose h
-          fd <- openFd path ReadOnly Nothing defaultFileFlags{ nonBlock = True }
-          ref <- newIORef False
-          f <- wrap $ \_ -> writeIORef ref True
+#ifdef mingw32_HOST_OS
+    []
+#else
+    [ testCase "addInputHandler increases handler count" $ do
+        withReadFd $ \fd -> do
+          f <- wrap $ \_ -> return ()
+          ref1 <- newIORef (0 :: Int)
+          forIH_ inputHandlers $ \_ -> modifyIORef' ref1 (+1)
+          _ <- R.addInputHandler inputHandlers fd f 0
+          ref2 <- newIORef 0
+          forIH_ inputHandlers $ \_ -> modifyIORef' ref2 (+1)
+          n1 <- readIORef ref1
+          n2 <- readIORef ref2
+          n1 @?= n2 - 1
+          freeHaskellFunPtr f
+
+    , testCase "removeInputHandler decreases handler count" $ do
+        withReadFd $ \fd -> do
+          f <- wrap $ \_ -> return ()
           ih <- R.addInputHandler inputHandlers fd f 0
-          runRegion $ refresh
-          (@?= True) =<< readIORef ref
           (@?= True) =<< R.removeInputHandler inputHandlers ih
           freeHaskellFunPtr f
-          closeFd fd
-#endif
+
+    , testCase "file events (select)" $ do
+        withReadFd $ \fd -> do
+          ref <- newIORef False
+          f <- wrap $ \_ -> writeIORef ref True
+          _ <- R.addInputHandler inputHandlers fd f 0
+          runRegion $ refresh
+          (@?= True) =<< readIORef ref
+          freeHaskellFunPtr f
     ]
+  where
+    withReadFd :: (Fd -> IO ()) -> IO ()
+    withReadFd action =
+      withSystemTempFile "inline-r-" $ \path h -> do
+        hPutStrLn h "hello"
+        hClose h
+        fd <- openFd path ReadOnly Nothing defaultFileFlags{ nonBlock = True }
+        action fd
+        closeFd fd
+#endif
