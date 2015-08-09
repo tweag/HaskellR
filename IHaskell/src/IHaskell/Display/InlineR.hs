b5299f1a2dacda0b43df8b58f3d1ed1b5dd1322d
Mathieu Boespflug <m@tweag.io>
2015-08-09 17:44:16 +0200

    IHaskell: Don't store pics in a local folder. Use a temp one.

--- IHaskell/src/IHaskell/Display/InlineR.hs
+++ IHaskell/src/IHaskell/Display/InlineR.hs
@@ -15,7 +15,8 @@ module IHaskell.Display.InlineR
   ) where
 
 import           Control.Applicative
-import qualified Data.ByteString as B
+import           Control.Monad (when)
+import qualified Data.ByteString as BS
 import qualified Data.ByteString.Base64 as Base64
 import qualified Data.ByteString.Char8 as Char
 import           Data.Monoid
@@ -25,7 +26,8 @@ import           IHaskell.Display.Blaze () -- to confirm it's installed
 import           Language.Haskell.TH.Quote
 import           Language.R.Instance
 import           Language.R.QQ
-import           System.Directory
+import           System.IO (hClose)
+import           System.IO.Temp (withSystemTempFile)
 import qualified Text.Blaze.Html5 as BH
 import qualified Text.Blaze.Html5.Attributes as BH
 
@@ -34,29 +36,21 @@ rprint = QuasiQuoter { quoteExp = \s -> [| do H.p $(quoteExp r s) |] }
 
 rgraph :: QuasiQuoter
 rgraph = QuasiQuoter { quoteExp = \s ->
-    [| do idx <- findMaxIndex 0
-          let fname = mkName idx
-          _ <- [r| png(filename=fname_hs, width=480, height=480, bg="white"); |]
+    [| withSystemTempFile "ihaskell-inline-r-.png" $ \path h -> do
+          hClose h
+          _ <- [r| png(filename=path_hs, width=480, height=480, bg="white"); |]
           H.p $(quoteExp r s)
           _ <- [r| dev.off() |]
-          encoded <- Base64.encode <$> B.readFile fname
-          display $ BH.img BH.! BH.src
-                                 (BH.unsafeByteStringValue
-                                    (Char.pack "data:image/png;base64," <> encoded))
+          encoded <- Base64.encode <$> BS.readFile path
+          when (BS.null encoded) $
+            fail "No graphical output."
+          display $
+            BH.img BH.!
+              BH.src (BH.unsafeByteStringValue
+                        (Char.pack "data:image/png;base64," <> encoded))
      |] }
-
-mkName :: Int -> FilePath
-mkName i = "Rplots/auto" <> Prelude.show i <> ".png"
-
-findMaxIndex :: Int -> IO Int
-findMaxIndex i = do
-  b <- doesFileExist $ mkName i
-  if b then findMaxIndex (i+1)
-       else return i
 
 -- | Initialize Embedded R process and configure R
 initializeEmbeddedR :: Config -> IO ()
 initializeEmbeddedR config = do
   H.initialize config
-  -- Configure plots output
-  createDirectoryIfMissing False "Rplots"
