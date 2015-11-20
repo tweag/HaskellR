039827e9f51c221591777668698bce4e76fa5a55
YPares <yves.pares@gmail.com>
2015-11-20 19:24:45 +0100

    Added a comment about where to find the modified nixpkgs

--- shell-from-nixpkgs-clone.nix
+++ shell-from-nixpkgs-clone.nix
@@ -1,9 +1,9 @@
-let pkgs = (import ~/fix/nixpkgs {});
+let pkgs = (import ~/fix/nixpkgs {});  # Cloned from https://github.com/datakurre/nixpkgs.git, branch datakurre-R (PR still not accepted in nixpkgs master)
 in
 with pkgs; stdenv.mkDerivation {
   name = "myEnv";
   buildInputs = [ python34Packages.ipython R zeromq pkgconfig zlib ghc ];
-  #STACK_IN_NIX_EXTRA_ARGS=''--extra-lib-dirs=${R}/lib/R/lib --extra-include-dirs=${R}/lib/R/include --extra-lib-dirs=${ghc}/lib --extra-include-dirs=${ghc}/include'';
+  STACK_IN_NIX_EXTRA_ARGS=''--extra-lib-dirs=${R}/lib/R/lib --extra-include-dirs=${R}/lib/R/include --extra-lib-dirs=${ghc}/lib --extra-include-dirs=${ghc}/include'';
   shellHook=''
     export DYLD_LIBRARY_PATH="${R}/lib/R/lib"
   '';
