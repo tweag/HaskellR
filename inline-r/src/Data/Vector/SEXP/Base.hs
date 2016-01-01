e5a81967af079570dc5fece41fb74d325cc00e7e
Mathieu Boespflug <m@tweag.io>
2016-01-01 22:23:20 +0100

    Make ElemRep a closed type family. For injectivity.

--- inline-r/src/Data/Vector/SEXP/Base.hs
+++ inline-r/src/Data/Vector/SEXP/Base.hs
@@ -20,16 +20,16 @@ import Foreign.Storable (Storable)
 
 -- | Function from R types to the types of the representations of each element
 -- in the vector.
-type family ElemRep s (a :: SEXPTYPE)
-type instance ElemRep s 'Char    = Word8
-type instance ElemRep s 'Logical = Logical
-type instance ElemRep s 'Int     = Int32
-type instance ElemRep s 'Real    = Double
-type instance ElemRep s 'Complex = Complex Double
-type instance ElemRep s 'String  = SEXP s 'Char
-type instance ElemRep s 'Vector  = SomeSEXP s
-type instance ElemRep s 'Expr    = SomeSEXP s
-type instance ElemRep s 'Raw     = Word8
+type family ElemRep s (a :: SEXPTYPE) where
+  ElemRep s 'Char    = Word8
+  ElemRep s 'Logical = Logical
+  ElemRep s 'Int     = Int32
+  ElemRep s 'Real    = Double
+  ElemRep s 'Complex = Complex Double
+  ElemRep s 'String  = SEXP s 'Char
+  ElemRep s 'Vector  = SomeSEXP s
+  ElemRep s 'Expr    = SomeSEXP s
+  ElemRep s 'Raw     = Word8
 
 -- | 'ElemRep' in the form of a relation, for convenience.
 type E s a b = ElemRep s a ~ b
