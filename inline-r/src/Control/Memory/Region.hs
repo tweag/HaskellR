3edf2ad5470695056ca1986b529ef87dd710917c
Mathieu Boespflug <m@tweag.io>
2016-01-23 16:57:19 +0100

    Map GlobalRegion to RealWorld.

--- inline-r/src/Control/Memory/Region.hs
+++ inline-r/src/Control/Memory/Region.hs
@@ -11,13 +11,13 @@
 
 module Control.Memory.Region where
 
-import GHC.Exts (Constraint)
+import GHC.Exts (Constraint, RealWorld)
 
 -- | The global region is a special region whose scope extends all the way to
 -- the end of the program. As such, any object allocated within this region
 -- lives "forever". In this sense, it is the top-level region, whose scope
 -- includes all other regions.
-data GlobalRegion
+type GlobalRegion = RealWorld
 
 -- | Void is not a region. It is a placeholder marking the absence of region.
 -- Useful to tag objects that belong to no region at all.
