6cc24f9f09abbd0a958585ab97c182347c9175ec
Mathieu Boespflug <m@tweag.io>
2016-09-04 21:53:54 +0200

    Support having Stack pass ghc as an argument to the nix shell file.

--- shell.nix
+++ shell.nix
@@ -1,10 +1,10 @@
+{pkgs ? import <nixpkgs> { }, ghc ? pkgs.haskell.compiler.ghc}:
+
 # A shell file to build HaskellR against a version of R with
 # the --enable-strict-barrier configure flag enabled for better memory
 # diagnostics.
 
-with (import <nixpkgs> { });
-
-with stdenv.lib;
+with pkgs;
 
 let
   R = pkgs.R.override { enableStrictBarrier = true; };
@@ -12,6 +12,7 @@ in
 
 haskell.lib.buildStackProject {
   name = "HaskellR";
-  buildInputs = [ ncurses pkgconfig python34Packages.ipython R zeromq zlib ];
+  inherit ghc;
+  buildInputs = [ ncurses python34Packages.ipython R zeromq zlib ];
   LANG = "en_US.UTF-8";
 }
