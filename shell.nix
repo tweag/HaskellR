6b41d13c145f21d66e6c5bce9f703df5acc19b35
Mathieu Boespflug <m@tweag.io>
2016-09-20 18:03:40 +0200

    Use just the shell file for Nix.
    Drop the packages section, which was being duplicated (and did diverge)
    between the stack.yaml and shell.nix.

--- shell.nix
+++ shell.nix
@@ -1,18 +1,27 @@
 {pkgs ? import <nixpkgs> { }, ghc ? pkgs.haskell.compiler.ghc}:
 
-# A shell file to build HaskellR against a version of R with
-# the --enable-strict-barrier configure flag enabled for better memory
-# diagnostics.
-
 with pkgs;
 
 let
-  R = pkgs.R.override { enableStrictBarrier = true; };
+  # Uncomment the line below to build HaskellR against a version of R with
+  # the --enable-strict-barrier configure flag enabled for better memory
+  # diagnostics.
+
+  # R = pkgs.R.override { enableStrictBarrier = true; };
 in
 
 haskell.lib.buildStackProject {
   name = "HaskellR";
   inherit ghc;
-  buildInputs = [ ncurses python34Packages.ipython R zeromq zlib ];
+  buildInputs =
+    [ ncurses
+      python34Packages.ipython
+      python34Packages.jupyter_client
+      python34Packages.notebook
+      R
+      zeromq
+      zlib
+    ];
   LANG = "en_US.UTF-8";
+  LD_LIBRARY_PATH = ["${R}/lib/R/"];
 }
