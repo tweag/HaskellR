26786d8fa566eba6a4ecabd1b0085b58d4afadd5
Alexander Vershilov <alexander.vershilov@gmail.com>
2018-12-24 00:43:56 +0300

    Fix mandelbrot-noout.R

--- inline-r/tests/shootout/mandelbrot-noout.R
+++ inline-r/tests/shootout/mandelbrot-noout.R
@@ -12,6 +12,9 @@ iter <- 50
 options ( warn = -1)
 
 mandelbrot_noout <- function(args) {
+    # Turn off warnings that appear on Windows, so that we can compare
+    # the output without the warning messages.
+    options ( warn = -1)
     n = if (length(args)) as.integer(args[[1]]) else 200L
     n_mod8 = n %% 8L
     pads <- if (n_mod8) rep.int(0, 8L - n_mod8) else integer(0)
@@ -19,16 +22,15 @@ mandelbrot_noout <- function(args) {
 
     cat("P4\n")
     cat(n, n, "\n")
-    bin_con <- pipe("cat", "wb")
     for (y in 0:(n-1)) {
         c <- 2 * 0:(n-1) / n - 1.5 + 1i * (2 * y / n - 1)
         z <- rep(0+0i, n)
         i <- 0L
-        while (i < iter) {  # faster than for loop
+        while (i < 50) {  # faster than for loop
             z <- z * z + c
             i <- i + 1L
         }
-        bits <- as.integer(abs(z) <= lim)
+        bits <- as.integer(abs(z) <= 2)
         bytes <- as.raw(colSums(matrix(c(bits * p, pads), 8L)))
     }
 }
