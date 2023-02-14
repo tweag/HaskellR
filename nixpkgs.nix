4774fcdd8852e58ba95e7705b905a316fa527ba1
Connor Baker <connor.baker@tweag.io>
2023-02-14 13:17:14 -0500

    nixpkgs.nix: 2022-11-27 -> 2023-02-14

--- nixpkgs.nix
+++ nixpkgs.nix
@@ -1,9 +1,9 @@
 let
-  # NixOS/Nixpkgs master 2022-11-27
-  rev = "a115bb9bd56831941be3776c8a94005867f316a7";
-  sha256 = "1501jzl4661qwr45b9ip7c7bpmbl94816draybhh60s9wgxn068d";
+  # NixOS/Nixpkgs master 2023-02-14
+  rev = "3bee4ab8db244db328b41ae5f4fbce19b658f1be";
+  sha256 = "1ibdbknqc0mvn1jww4vhqb709sl1k3yzazx907snczm0f17grcr0";
 in
-import (fetchTarball {
-  inherit sha256;
-  url = "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
-})
+  import (fetchTarball {
+    inherit sha256;
+    url = "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
+  })
