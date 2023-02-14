f8a8063f01cc6c00f03591d1aed7dc0e93b47e70
Connor Baker <connor.baker@tweag.io>
2023-02-14 13:11:47 -0500

    shell.nix: Refactor to remove `with` and redundant LD_LIBRARY_PATH

--- shell.nix
+++ shell.nix
@@ -1,33 +1,44 @@
-{pkgs ? import ./nixpkgs.nix { }, ghc ? pkgs.haskell.compiler.ghc8104}:
-
-with pkgs;
-
-let
+{
+  pkgs ? import ./nixpkgs.nix {},
+  stdenv ? pkgs.stdenv,
+  buildStackProject ? pkgs.haskell.lib.buildStackProject,
+  ghc ? pkgs.haskell.compiler.ghc8107,
+  R ? pkgs.R,
+  zeromq ? pkgs.zeromq,
+  zlib ? pkgs.zlib,
+  ipython ? pkgs.python3Packages.ipython,
+  jupyter_client ? pkgs.python3Packages.jupyter_client,
+  notebook ? pkgs.python3Packages.notebook,
+}: let
   # Uncomment the line below to build HaskellR against a version of R with
   # the --enable-strict-barrier configure flag enabled for better memory
   # diagnostics.
-
   # R = pkgs.R.override { enableStrictBarrier = true; };
-
   # XXX Workaround https://ghc.haskell.org/trac/ghc/ticket/11042.
-  libHack = if stdenv.isDarwin then {
+  libHack =
+    if stdenv.isDarwin
+    then {
       DYLD_LIBRARY_PATH = ["${R}/lib/R/lib"];
-    } else {
+    }
+    else {
       LD_LIBRARY_PATH = ["${R}/lib/R"];
     };
 in
+  buildStackProject ({
+      name = "HaskellR";
+      inherit ghc;
+      buildInputs = [
+        R
 
-haskell.lib.buildStackProject ({
-  name = "HaskellR";
-  inherit ghc;
-  buildInputs =
-    [ python3Packages.ipython
-      python3Packages.jupyter_client
-      python3Packages.notebook
-      R
-      zeromq
-      zlib
-    ];
-  LANG = "en_US.UTF-8";
-  LD_LIBRARY_PATH = ["${R}/lib/R/"];
-} // libHack)
+        # Libraries
+        zeromq
+        zlib
+
+        # Python packages
+        ipython
+        jupyter_client
+        notebook
+      ];
+      LANG = "en_US.UTF-8";
+    }
+    // libHack)
