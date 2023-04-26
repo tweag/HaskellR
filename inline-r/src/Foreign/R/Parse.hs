72642330ad38d441f113d991c93c44bdace01c6a
Facundo Domínguez <facundo.dominguez@tweag.io>
2023-04-26 09:47:44 -0300

    Make Parse.hsc a regular module Parse.hs
    Liquid Haskell can't collect specs in .hsc files.
    See https://github.com/ucsd-progsys/liquidhaskell/issues/2132

--- inline-r/src/Foreign/R/Parse.hs
+++ inline-r/src/Foreign/R/Parse.hs
@@ -3,48 +3,22 @@
 --
 -- Bindings for @<R/R_ext/Parse.h>@.
 
-{-# LANGUAGE CPP #-}
 {-# LANGUAGE CApiFFI #-}
 {-# LANGUAGE ConstraintKinds #-}
 {-# LANGUAGE ViewPatterns #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE ForeignFunctionInterface #-}
 
-#include <Rinternals.h>
-#include <R_ext/Parse.h>
 module Foreign.R.Parse
   ( parseVector
   , ParseStatus(..)
   ) where
 
+import Foreign.R.Type (ParseStatus(..))
 import qualified Foreign.R as R
 
 import Foreign
 import Foreign.C
-
--- | The return code of a call to 'parseVector', indicating whether the parser
--- failed or succeeded.
-data ParseStatus
-  = PARSE_NULL
-  | PARSE_OK
-  | PARSE_INCOMPLETE
-  | PARSE_ERROR
-  | PARSE_EOF
-  deriving (Eq, Show)
-
-instance Enum ParseStatus where
-  fromEnum PARSE_NULL       = #const PARSE_NULL
-  fromEnum PARSE_OK         = #const PARSE_OK
-  fromEnum PARSE_INCOMPLETE = #const PARSE_INCOMPLETE
-  fromEnum PARSE_ERROR      = #const PARSE_ERROR
-  fromEnum PARSE_EOF        = #const PARSE_EOF
-  toEnum i = case i of
-    (#const PARSE_NULL)       -> PARSE_NULL
-    (#const PARSE_OK)         -> PARSE_OK
-    (#const PARSE_INCOMPLETE) -> PARSE_INCOMPLETE
-    (#const PARSE_ERROR)      -> PARSE_ERROR
-    (#const PARSE_EOF)        -> PARSE_EOF
-    _ -> error "ParseStatus.fromEnum: can't mach value"
 
 -- | @parseVector text num status source@ parses the input string into an AST.
 -- @source@, if provided, names the origin of @text@ (e.g. a filename). @num@
@@ -59,6 +33,6 @@ parseVector
   -> IO (R.SEXP s)
 parseVector (R.unsexp -> s) (fromIntegral -> cnt) reti (R.unsexp -> input) =
   R.sexp <$> c_parseVector s cnt reti input
- 
+
 foreign import ccall "R_ext/Parse.h R_ParseVector" c_parseVector
   :: R.SEXP0 -> CInt -> Ptr CInt -> R.SEXP0 -> IO R.SEXP0
